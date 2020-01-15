1b122bc68d8c56bd1f407ee5bd7da3624d0b5d71
Olivier Aubert <contact@olivieraubert.net>
2020-01-15 22:53:33 +0100

    Remove quasar dependency for now

--- js/Debug.vue
+++ js/Debug.vue
@@ -6,11 +6,10 @@
     <div v-touch-pan="handler">v1: {{ v1 }}</div>
     <div v-touch-swipe="handler">v2: {{ v2 }}</div>
     <span>userdata: {{ userdata }}</span>
-    <div v-ripple>Click me. I got ripples.
     </div>
     <input v-model="userdata">
     <div>
-      <q-btn @click="doSomething" label="Do something" />
+      <button @click="doSomething" label="Do something" />
     </div>
 </template>
 
