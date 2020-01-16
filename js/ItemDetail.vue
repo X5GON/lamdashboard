2c4a4e438958c8dcd01f9425991893007fe0df54
Olivier Aubert <contact@olivieraubert.net>
2020-01-16 16:33:50 +0100

    Add difficulty/length hints on representation

--- js/ItemDetail.vue
+++ js/ItemDetail.vue
@@ -1,39 +1,79 @@
 <template>
   <div class="view-itemdetail">
-    <div class="itemdetail-name">{{ item.name }}</div>
-    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 50 100" preserveAspectRatio="xMidYMid">
-      <defs>
-        <radialGradient id="paint0_radial" cx="0" cy="0" r="100%" fx="0" fy="0" gradientUnits="objectBoundingBox" transform="translate(100%)" spreadMethod="pad">
-      <stop stop-color="#ff9059" stop-opacity="1" offset="0"/>
-      <stop stop-color="#ff9059" stop-opacity="0.2" offset="1"/>
-        </radialGradient>
-      </defs>
-      <rect x="0" y="0" width="50" height="100" stroke="black" stroke-opacity="1" stroke-width=".1" fill="none" />
-      <g transform="translate(25, 75) scale(1,-1)">
-        <circle id="circle" fill="url(#paint0_radial)" fill-opacity="1" fill-rule="nonzero" stroke="none" cx="0" cy="0" r="25"/>
-        <path id="concept1" fill="none" stroke="#3e1966" stroke-linecap="round" stroke-opacity="1" stroke-width="5" d="M 0 0 L 0 15"/>
-        <path id="concept2" fill="none" stroke="#933b50" stroke-linecap="round" stroke-opacity="1" stroke-width="5" d="M 0 15 L 0 20"/>
-        <path id="concept3" fill="none" stroke="#be4b4d" stroke-linecap="round" stroke-opacity="1" stroke-width="5" d="M 0 20 L 0 30"/>
-        <path id="concept4" fill="none" stroke="#e95c44" stroke-linecap="round" stroke-opacity="1" stroke-width="5" d="M 0 30 L 0 40"/>
-        <path id="concept5" fill="none" stroke="#505973" stroke-linecap="round" stroke-opacity="1" stroke-width="5" d="M 0 40 L 0 45"/>
-      </g>
-    </svg>
+    <div class="itemdetail-name">{{ item ? item.name : "" }}</div>
+    <div v-if="item" class="itemdetail-representation">
+      <svg xmlns="http://www.w3.org/2000/svg" width="400" height="600" viewBox="0 0 110 120" preserveAspectRatio="xMidYMid">
+        <defs>
+          <radialGradient id="paint0_radial">
+            <stop stop-color="#ff9059" stop-opacity="1" offset="0"/>
+            <stop stop-color="#ff9059" stop-opacity="0.2" offset="1"/>
+          </radialGradient>
+          <marker id="triangle" viewBox="0 0 20 20"
+                  refX="10" refY="10"
+                  markerWidth="20" markerHeight="20"
+                  orient="auto-start-reverse">
+            <path d="M 0 0 l 10 10 l -10 10" stroke="#505973" fill="none" />
+          </marker>
+        </defs>
+        <g transform="translate(75, 75) scale(1,-1)">
+          <circle id="circle" fill="url(#paint0_radial)" fill-opacity="1" fill-rule="nonzero" stroke="none" cx="0" cy="0" r="25"/>
+          <path id="concept1" fill="none" stroke="#3e1966" stroke-linecap="round" stroke-opacity="1" stroke-width="4" d="M 0 0 L 0 15"/>
+          <path id="concept2" fill="none" stroke="#933b50" stroke-linecap="round" stroke-opacity="1" stroke-width="4" d="M 0 15 L 0 20"/>
+          <path id="concept3" fill="none" stroke="#be4b4d" stroke-linecap="round" stroke-opacity="1" stroke-width="4" d="M 0 20 L 0 30"/>
+          <path id="concept4" fill="none" stroke="#e95c44" stroke-linecap="round" stroke-opacity="1" stroke-width="4" d="M 0 30 L 0 40"/>
+          <path id="concept5" fill="none" stroke="#505973" stroke-linecap="round" stroke-opacity="1" stroke-width="4" d="M 0 40 L 0 45"/>
+          <g id="difficulty_hint">
+            <path fill="none" stroke="#505973" stroke-width=".15" stroke-dasharray=".5 1" d="M 0 47 l -60 0" />
+            <path fill="none" stroke="#505973" stroke-width=".15" stroke-dasharray=".5 1" d="M 0 -2 l -60 0" />
+            <path fill="none" stroke="#505973" stroke-width=".15" d="M -60 -2 l 0 49" marker-start="url(#triangle)" marker-end="url(#triangle)" />
+            <text transform="translate(-61, 15) rotate(90) scale(1,-1)" fill="#505973" font-family="Open Sans" font-size="3">DIFFICULTY</text>
+          </g>
+          <g id="length_hint">
+            <path fill="none" stroke="#505973" stroke-width=".15" stroke-dasharray=".5 1" d="M -25 -2 l 0 -30" />
+            <path fill="none" stroke="#505973" stroke-width=".15" stroke-dasharray=".5 1" d="M 25 -2 l 0 -30" />
+            <path fill="none" stroke="#505973" stroke-width=".15" d="M -25 -30 l 50 0" marker-start="url(#triangle)" marker-end="url(#triangle)" />
+            <text transform="translate(-6, -34) scale(1,-1)" fill="#505973" font-family="Open Sans" font-size="3">LENGTH</text>
+          </g>
+        </g>
+      </svg>
+    </div>
+    <div class="itemdetail-provider">{{ item ? item.provider : "" }}</div>
+    <div class="itemdetail-author">{{ item ? item.author : "" }}</div>
+
   </div>
 </template>
 
 <script>
-  module.exports = {
-      data: function() {
-          return {
-          }
-      },
-      props: {
-          item: Object
-      }
-      methods: {
-      }
-  }
+    module.exports = {
+        data: function() {
+            return {
+            }
+        },
+        props: {
+            item: Object
+        },
+        methods: {
+        }
+    };
 </script>
 
 <style scoped>
+  .view-itemdetail {
+    width: calc(100% - 70px);
+    padding-left: 35px;
+    padding-right: 35px;
+  }
+  .itemdetail-name {
+      color: #fff;
+      font-family: IBM Plex Serif;
+      font-style: normal;
+      font-weight: 500;
+      font-size: 26;
+      line-height: 31px;
+  }
+
+  .itemdetail-representation {
+    margin-left: 30%;
+    width: 30%;
+  }
 </style>
