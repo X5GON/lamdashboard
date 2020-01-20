60cc7ab92b9b80fa3569b6102c5b74e89640b49e
Olivier Aubert <contact@olivieraubert.net>
2020-01-20 18:45:17 +0100

    Remove debug data
    It is now stored in json files

--- js/Overview.vue
+++ js/Overview.vue
@@ -42,61 +42,12 @@
                 // Get the 5 top concepts of the resource
                 if (this.active_resource === null)
                     return null;
-                return [
-                    [
-                        "Voxel",
-                        "http://en.wikipedia.org/wiki/Voxel",
-                        0.007178634185829779,
-                        0.15250415330651737
-                    ],
-                    [
-                        "Brain",
-                        "http://en.wikipedia.org/wiki/Brain",
-                        0.007178634185829779,
-                        0.3948504501157241
-                    ],
-                    [
-                        "Neuroscience",
-                        "http://en.wikipedia.org/wiki/Neuroscience",
-                        0.003918594338552176,
-                        0.2598512031787645
-                    ],
-                    [
-                        "Neuron",
-                        "http://en.wikipedia.org/wiki/Neuron",
-                        0.003190581832126049,
-                        0.1680247074361474
-                    ],
-                    [
-                        "Stimulus (physiology)",
-                        "http://en.wikipedia.org/wiki/Stimulus_(physiology)",
-                        0.003178383788377945,
-                        0.30299689254684864
-                    ],
-                    [
-                        "Overfitting",
-                        "http://en.wikipedia.org/wiki/Overfitting",
-                        0.0029340111255755563,
-                        0.1628208589786088
-                    ],
-                    [
-                        "Computer vision",
-                        "http://en.wikipedia.org/wiki/Computer_vision",
-                        0.0023786203322898694,
-                        0.1047853803949352
-                    ],
-                    [
-                        "Computational model",
-                        "http://en.wikipedia.org/wiki/Computational_model",
-                        0.0021685764506235686,
-                        0.15095315172810972
-                    ]
-                ].slice(0, 5).map((t, i) => ({ label: t[0],
-                                               url: t[1],
-                                               value: t[3],
-                                               width: t[3] * 200,
-                                               color: [ "#3e1966", "#933b50", "#be4b4d", "#e95c44", "#505973" ][i]
-                                             }));
+                return this.active_resource.wikifier.slice(0, 5).map((t, i) => ({ label: t[0],
+                                                                                  url: t[1],
+                                                                                  value: t[3],
+                                                                                  width: t[3] * 200,
+                                                                                  color: [ "#3e1966", "#933b50", "#be4b4d", "#e95c44", "#505973" ][i]
+                                                                                }));
             }
         }
     }
