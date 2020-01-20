f45d655164f4cd519d55997d1d09720e16450887
Olivier Aubert <contact@olivieraubert.net>
2020-01-20 23:47:04 +0100

    Factorize SvgContainer

--- js/ItemDetail.vue
+++ js/ItemDetail.vue
@@ -2,22 +2,9 @@
   <div v-if="item" class="view-itemdetail">
     <div class="itemdetail-title">{{ item ? item.title : "" }}</div>
     <div class="itemdetail-representation">
-      <svg xmlns="http://www.w3.org/2000/svg" :width="svg_width()" viewBox="0 0 110 130" preserveAspectRatio="xMidYMid">
-        <defs>
-          <radialGradient id="paint0_radial">
-            <stop stop-color="#ff9059" stop-opacity="1" offset="0"/>
-            <stop stop-color="#ff9059" stop-opacity="0.2" offset="1"/>
-          </radialGradient>
-          <marker id="triangle" viewBox="0 0 20 20"
-                  refX="10" refY="10"
-                  markerWidth="20" markerHeight="20"
-                  orient="auto-start-reverse">
-            <path d="M 0 0 l 10 10 l -10 10" stroke="#505973" fill="none" />
-          </marker>
-        </defs>
-        <rect x="0" y="0" width="100%" height="100%" stroke="none" stroke-opacity="1" stroke-width=".1" fill="none" />
-        <resource-representation :item="item"></resource-representation>
-      </svg>
+      <svg-container>
+        <resource-representation legend :item="item"></resource-representation>
+      </svg-container>
     </div>
     <div class="itemdetail-provider">{{ item ? item.provider : "" }}</div>
     <div class="itemdetail-author">{{ item ? item.author : "" }}</div>
@@ -30,12 +17,8 @@
             item: Object,
         },
         components: {
+            'svg-container': httpVueLoader('js/SvgContainer.vue'),
             'resource-representation': httpVueLoader('js/ResourceRepresentation.vue'),
-        },
-        methods: {
-            svg_width: function () {
-                return this.$el.clientWidth * .8;
-            },
         },
     };
 </script>
---
+++ js/SvgContainer.vue
@@ -0,0 +1,31 @@
+<template>
+  <svg xmlns="http://www.w3.org/2000/svg" :width="svg_width()" viewBox="0 0 110 130" preserveAspectRatio="xMidYMid">
+    <defs>
+      <radialGradient id="paint0_radial">
+        <stop stop-color="#ff9059" stop-opacity="1" offset="0"/>
+        <stop stop-color="#ff9059" stop-opacity="0.2" offset="1"/>
+      </radialGradient>
+      <marker id="triangle" viewBox="0 0 20 20"
+              refX="10" refY="10"
+              markerWidth="20" markerHeight="20"
+              orient="auto-start-reverse">
+        <path d="M 0 0 l 10 10 l -10 10" stroke="#505973" fill="none" />
+      </marker>
+    </defs>
+    <rect x="0" y="0" width="100%" height="100%" stroke="none" stroke-opacity="1" stroke-width=".1" fill="none" />
+    <slot></slot>
+  </svg>
+</template>
+
+<script>
+    module.exports = {
+        methods: {
+            svg_width: function () {
+                return this.$el.clientWidth;
+            },
+        },
+    };
+</script>
+
+<style scoped>
+</style>
