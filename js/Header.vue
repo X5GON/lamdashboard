28aa27af870e75169934a452af6abdb44e9a5e47
Olivier Aubert <contact@olivieraubert.net>
2020-01-22 17:01:37 +0100

    Define .overlay class (transparent, full width)

--- js/Header.vue
+++ js/Header.vue
@@ -15,4 +15,12 @@
     align-items: center;
     justify-content: center;
   }
+
+  header.x5gon-header.overlay {
+    background-color: #fffa;
+    justify-content: left;
+    background: linear-gradient(180deg, #fff -55%, #fff0 100%);
+    padding-left: 20px;
+    opacity: .7;
+  }
 </style>
