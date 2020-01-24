2ea8716b408b48b2814edfe5faef1d6ee31c7e76
Olivier Aubert <contact@olivieraubert.net>
2020-01-24 17:20:42 +0100

    Make component registration generic

--- js/Components.js
+++ js/Components.js
@@ -1,15 +1,29 @@
 export default {
+    ...Object.fromEntries([
+        // Main view components
+        "Home",
+        "Search",
+        "Overview",
+        "Basket",
+        "Sequence",
+        "Debug",
+
+        // Other components (alphabetical order)
+        "BarLegend",
+        "ConceptBar",
+        "ItemDetail",
+        "LetterCube",
+        "NeighborGraph",
+        "ResourceRepresentation",
+        "SvgContainer"
+    ].map(name => ([ name,
+                     // We convert camelCase to kebab-case for component names
+                     Vue.component(name
+                                   .replace(/^[A-Z]/, m => m.toLowerCase())
+                                   .replace(/[A-Z]/g, m => "-" + m.toLowerCase()),
+                                   httpVueLoader.load(`js/${name}.vue`))
+                   ]))),
+    // Specific prefixed components
     Notification: Vue.component('x5gon-notification', httpVueLoader('js/Notification.vue')),
     LoadingSpinner: Vue.component('x5gon-loading-spinner', httpVueLoader('js/LoadingSpinner.vue')),
-    LetterCube: Vue.component('letter-cube', httpVueLoader.load('js/LetterCube.vue')),
-
-    // Main view components
-    Home: httpVueLoader('js/Home.vue'),
-    Search: httpVueLoader('js/Search.vue'),
-    Overview: httpVueLoader('js/Overview.vue'),
-    Basket: httpVueLoader('js/Basket.vue'),
-    Sequence: httpVueLoader('js/Sequence.vue'),
-    Debug: httpVueLoader('js/Debug.vue'),
-
-    
 }
