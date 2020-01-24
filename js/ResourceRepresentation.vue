94fca1ee16a1c40eb44cdb0be3d00644d8d9f185
Olivier Aubert <contact@olivieraubert.net>
2020-01-24 14:46:51 +0100

    Add max_width/max_height props

--- js/ResourceRepresentation.vue
+++ js/ResourceRepresentation.vue
@@ -1,5 +1,5 @@
 <template>
-  <g @mouseover="on_mouseover" @click="on_click"
+  <g @mouseover="on_mouseover" @click="on_click" @dblclick="on_dblclick"
      :transform="`translate(${x}, ${y}) scale(1,-1)`">
     <circle id="circle" fill="url(#paint0_radial)" fill-opacity="1" fill-rule="nonzero" stroke="none" cx="0" cy="0" :r="radius"></circle>
     <path v-for="(item, index) in reversed_concepts"
@@ -11,16 +11,32 @@
     <text v-if="title" :transform="`translate(-4, 0) rotate(90) scale(1,-1)`" fill="#fff" font-family="Open Sans" font-size="4">{{ title }}</text>
     <g v-if="legend" id="legend">
       <g id="difficulty_hint">
-        <path fill="none" stroke="#505973" stroke-width=".15" stroke-dasharray=".5 1" :d="`M 0 ${bar_height+2} l -60 0`"></path>
-        <path fill="none" stroke="#505973" stroke-width=".15" stroke-dasharray=".5 1" d="M 0 -2 l -60 0"></path>
-        <path fill="none" stroke="#505973" stroke-width=".15" :d="`M -60 -2 l 0 ${bar_height+4}`" marker-start="url(#triangle)" marker-end="url(#triangle)"></path>
-        <text :transform="`translate(-61, ${bar_height/2-10}) rotate(90) scale(1,-1)`" fill="#505973" font-family="Open Sans" font-size="3">DIFFICULTY</text>
+        <path fill="none" stroke="#505973" stroke-width=".15" stroke-dasharray=".5 1"
+              :d="`M 0 ${bar_height+2} l -${difficulty_legend_offset} 0`"></path>
+        <path fill="none" stroke="#505973" stroke-width=".15" stroke-dasharray=".5 1"
+              :d="`M 0 -2 l -${difficulty_legend_offset} 0`"></path>
+        <path fill="none" stroke="#505973" stroke-width=".15"
+              :d="`M -${difficulty_legend_offset} -2 l 0 ${bar_height+4}`"
+              marker-start="url(#triangle)"
+              marker-end="url(#triangle)"></path>
+        <text :transform="`translate(-${difficulty_legend_offset + 1}, ${bar_height/2-10}) rotate(90) scale(1,-1)`"
+              fill="#505973"
+              font-family="Open Sans"
+              font-size="3">DIFFICULTY</text>
       </g>
       <g id="length_hint">
-        <path fill="none" stroke="#505973" stroke-width=".15" stroke-dasharray=".5 1" :d="`M -${radius} -2 l 0 -${radius+5}`"></path>
-        <path fill="none" stroke="#505973" stroke-width=".15" stroke-dasharray=".5 1" :d="`M ${radius} -2 l 0 -${radius+5}`"></path>
-        <path fill="none" stroke="#505973" stroke-width=".15" :d="`M -${radius} -${radius+5} l ${2*radius} 0`" marker-start="url(#triangle)" marker-end="url(#triangle)"></path>
-        <text :transform="`translate(-6, -${radius+9}) scale(1,-1)`" fill="#505973" font-family="Open Sans" font-size="3">LENGTH</text>
+        <path fill="none" stroke="#505973" stroke-width=".15" stroke-dasharray=".5 1"
+              :d="`M -${radius} -2 l 0 -${radius+5}`"></path>
+        <path fill="none" stroke="#505973" stroke-width=".15" stroke-dasharray=".5 1"
+              :d="`M ${radius} -2 l 0 -${radius+5}`"></path>
+        <path fill="none" stroke="#505973" stroke-width=".15"
+              :d="`M -${radius} -${radius+5} l ${2*radius} 0`"
+              marker-start="url(#triangle)"
+              marker-end="url(#triangle)"></path>
+        <text :transform="`translate(-6, -${radius+9}) scale(1,-1)`"
+              fill="#505973"
+              font-family="Open Sans"
+              font-size="3">LENGTH</text>
       </g>
     </g>
   </g>
@@ -50,6 +66,14 @@
                 type: Number,
                 default: 40
             },
+            max_width: {
+                type: Number,
+                default: 50
+            },
+            max_height: {
+                type: Number,
+                default: 105
+            },
             concept_count: {
                 type: Number,
                 default: 5
@@ -73,17 +97,22 @@
             on_click: function () {
                 this.$emit("resource_click", this.item);
             },
+            on_dblclick: function () {
+                this.$emit("resource_dblclick", this.item);
+            },
         },
         computed: {
             duration_scale: function () {
+                // Map duration (in s) to radius (in px)
                 return d3.scaleLinear()
                     .domain([ 60, this.max_duration ])
-                    .range([ 20, 50 ]);
+                    .range([ 10, this.legend ? this.max_width + 30 : this.max_width ]);
             },
             difficulty_scale: function () {
+                // Map length (in w/s) to bar length (in px)
                 return d3.scaleLinear()
                     .domain([ 1, this.max_difficulty ])
-                    .range([ 40, 80 ]);
+                    .range([ 30, this.max_height - this.max_width / 2 ]);
             },
             radius: function () {
                 // FIXME: replace with item.duration when OK
@@ -95,6 +124,12 @@
             concepts: function () {
                 let count = this.concept_count;
                 return this.item ? this.item.wikifier.slice(0, count) : [];
+            },
+            difficulty_legend_offset: function () {
+                return this.max_width + 10;
+            },
+            length_legend_offset: function () {
+                return this.max_width / 2 + 10;
             },
             reversed_concepts: function () {
                 return this.concepts.slice().reverse();
