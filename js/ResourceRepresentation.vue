647a2c8e8a93c59fa4b03eea259a3da54629ec70
Olivier Aubert <contact@olivieraubert.net>
2020-02-13 10:42:57 +0100

    Fix ResourceRepresentation
    new data model + fix concept bar display (to have the concepts layed out top-down)

--- js/ResourceRepresentation.vue
+++ js/ResourceRepresentation.vue
@@ -5,16 +5,16 @@
      @dblclick="on_dblclick"
      :transform="`translate(${x}, ${y}) scale(1,-1)`">
 
-      <circle id="circle" fill="url(#paint0_radial)" :fill-opacity="is_reference ? 1 : 0" fill-rule="nonzero" :stroke="resource_stroke" stroke-width="3" cx="0" cy="0" :r="radius"></circle>
+      <circle id="circle" fill="url(#paint0_radial)" :fill-opacity="is_reference ? 1 : 0" fill-rule="nonzero" :stroke="resource_stroke" stroke-width="2" cx="0" cy="0" :r="radius"></circle>
 
       <g v-if="detailed_concepts">
-        <path v-for="(item, index) in reversed_concepts"
-              :id="`concept${concept_count-index}`"
+        <path v-for="(item, index) in concepts"
+              :id="`concept${index}`"
               @mouseover="on_concept_mouseover(item)"
               fill="none"
               stroke-opacity="1" stroke-width="6"
               :stroke="color_palette(item, index)"
-              :d="`M 0 0 l 0 ${y_concept[concept_count-index-1]}`"></path>
+              :d="`M 0 ${y_concept[index-1] || bar_height} L 0 ${y_concept[index]}`"></path>
       </g>
       <g v-else>
         <path stroke-opacity="1" stroke-width="4"
@@ -27,11 +27,11 @@
             transform="translate(-4, 0) rotate(90) scale(1,-1)"
             fill="#fff"
             font-family="Open Sans"
-            font-size="6">
+            font-size="4">
         {{ title }}
       </text>
       <g v-if="legend" id="legend">
-        <g id="difficulty_hint">
+        <g id="difficulty_legend">
           <path fill="none" stroke="#505973" stroke-width=".15" stroke-dasharray=".5 1"
                 :d="`M 0 ${bar_height+2} l -${difficulty_legend_offset} 0`"></path>
           <path fill="none" stroke="#505973" stroke-width=".15" stroke-dasharray=".5 1"
@@ -45,7 +45,7 @@
                 font-family="Open Sans"
                 font-size="3">DIFFICULTY</text>
       </g>
-        <g id="length_hint">
+        <g id="duration_legend">
           <path fill="none" stroke="#505973" stroke-width=".15" stroke-dasharray=".5 1"
                 :d="`M -${radius} -2 l 0 -${radius+5}`"></path>
           <path fill="none" stroke="#505973" stroke-width=".15" stroke-dasharray=".5 1"
@@ -57,7 +57,7 @@
           <text :transform="`translate(-6, -${radius+9}) scale(1,-1)`"
                 fill="#505973"
                 font-family="Open Sans"
-                font-size="3">LENGTH</text>
+                font-size="3">DURATION</text>
         </g>
       </g>
   </g>
@@ -145,26 +145,25 @@
                     .range([ 10, this.legend ? this.max_width + 30 : this.max_width ]);
             },
             difficulty_scale: function () {
-                // Map length (in w/s) to bar length (in px)
+                // Map difficulty (in w/s) to bar length (in px)
                 return d3.scaleLinear()
                     .domain([ 1, this.max_difficulty ])
                     .range([ 30, this.max_height - this.max_width / 2 ]);
             },
             radius: function () {
-                // FIXME: replace with item.duration when OK
-                return this.duration_scale(this.item ? this.item.length : 0);
+                return this.duration_scale(this.item ? this.item.duration : 0);
             },
             bar_height: function () {
-                return this.difficulty_scale(this.item.technicities);
+                return this.difficulty_scale(this.item.difficulty);
             },
             concepts: function () {
                 let count = this.concept_count;
                 return this.item ? this.item.wikifier.slice(0, count) : [];
             },
             difficulty_legend_offset: function () {
-                return this.max_width + 10;
+                return this.radius + 10;
             },
-            length_legend_offset: function () {
+            duration_legend_offset: function () {
                 return this.max_width / 2 + 10;
             },
             reversed_concepts: function () {
@@ -172,17 +171,19 @@
             },
             y_concept: function () {
                 // Return an array with stacked lengths
-                let values = this.concepts.map(c => c[3]);
+                let values = this.concepts.map(c => c.value);
                 let total = values.reduce((a, b) => a+b);
                 let h = this.bar_height;
                 let s = 0;
                 let heights = values.reduce((result, v) => { s = s + v / total;
-                                                             result.push(s  * h);
+                                                             result.push(s * h);
                                                              return result }, []);
-                return heights;
+                // Since we draw the bar from the top (and the y axis is bottom-up)
+                // let's reverse there the values
+                return heights.map(h => this.bar_height - h);
             },
             concept_classes: function () {
-                return this.concepts.map(c => `concept-${c[1].replace(/.*\//, '')}`)
+                return this.concepts.map(c => `concept-${c.label.replace(/.*\//, '')}`)
             },
             resource_stroke: function () {
                 if (this.is_reference) {
