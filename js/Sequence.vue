7f1c97dd108416c6d3b13607d37483564dc07f10
Olivier Aubert <contact@olivieraubert.net>
2020-02-21 19:37:45 +0100

    Add ResourceInformationPanel to Sequence view

--- js/Sequence.vue
+++ js/Sequence.vue
@@ -1,20 +1,29 @@
 <template>
-  <div class="view-sequence">
+  <div class="view-sequence" :class="{ fullscreen: is_fullscreen }">
     <x5gon-header class="overlay"></x5gon-header>
-    <h1>Sequence</h1>
-    <svg-container class="svg-content" v-if="items.length > 0" :key="items.length" :viewbox="`0 0 ${x_max} 250`">
-      <g>
-        <resource-representation v-for="(item, index) in positioned_items"
-                                 @resource_click="insert_resource(item)"
-                                 :x="item.x_position"
-                                 :y="100"
-                                 :is_suggested="item.is_suggested"
-                                 detailed_concepts
-                                 :key="`${index}-${item.url}`"
-                                 :title="item.title"
-                                 :item="item"></resource-representation>
-      </g>
-    </svg-container>
+    <div class="sequence-content">
+      <h1>Sequence</h1>
+      <svg-container class="svg-content" v-if="items.length > 0" :key="items.length" :viewbox="`0 0 ${x_max} 200`">
+        <g>
+          <resource-representation v-for="(item, index) in positioned_items"
+                                   @resource_click="insert_resource(item)"
+                                   @resource_mouseover="on_resource_mouseover"
+                                   :x="item.x_position"
+                                   :y="100"
+                                   :is_suggested="item.is_suggested"
+                                   detailed_concepts
+                                   :key="`${index}-${item.url}`"
+                                   :title="item.title"
+                                   :item="item"></resource-representation>
+        </g>
+      </svg-container>
+    </div>
+
+    <resource-information-panel v-if="active_resource"
+                                :resource="active_resource"
+                                :concept_palette="concept_palette"
+                                :is_fullscreen="is_fullscreen">
+    </resource-information-panel>
     <div class="sequence-menu right-drawer-menu">
       <ul class="sequence-menu-list">
         <li @click="do_organize"><img alt="" src="img/icon_organize.svg">Organize</li>
@@ -29,6 +38,13 @@
 <script>
   module.exports = {
       name: "Sequence",
+      data: function() {
+          return {
+              is_fullscreen: true,
+              active_resource: null,
+              palette: {},
+          }
+      },
       computed: {
           ...Vuex.mapState([ "sequence", "insertions" ]),
           items: function () {
@@ -68,6 +84,17 @@
 
       },
       methods: {
+          on_resource_mouseover: function (item) {
+              this.active_resource = item;
+          },
+          concept_palette: function (url) {
+              let color = this.palette[url];
+              if (! color) {
+                  color = this.$constant.palette.concepts.find(col => ! Object.values(this.palette).includes(col)) || '#000d32';
+                  this.palette[url] = color;
+              }
+              return color;
+          },
           show_basket: function () {
               this.$router.push('/basket');
           },
@@ -102,6 +129,9 @@
     font-size: 38px;
     line-height: 26px;
     color: #fff;
+  }
+  .fullscreen .sequence-content {
+      height: calc(100vh - 170px);
   }
   .sequence-list {
       margin: 0 10%;
